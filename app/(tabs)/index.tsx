7e7c2d686bde7babf815dee00d2d4b3258a380d8
apophis51 <malcolmxvernon@hotmail.com>
2025-02-13 23:25:11 -0500

    asdfad

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,79 +1,38 @@
-import { Image, StyleSheet, Platform, Text } from 'react-native';
-
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
-import { verifyInstallation } from 'nativewind';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { Audio } from 'expo-av';
+import { WebView } from 'react-native-webview';
+import Constants from 'expo-constants';
 
 
-export default function HomeScreen() {
-  verifyInstallation();
-  return (
-    <>
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <Text className="text-cyan-500 bg-black">gggvHdelfflo</Text>
-        <ThemedText type="title">dvsgf!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12'
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView></>
-  );
+export default function App() {
+    const [hasPermission, setPermission] = useState(false)
+
+
+
+    useEffect(() => {
+        (async () => {
+            await Audio.requestPermissionsAsync()
+            setPermission(true)
+
+        }
+
+        )();
+    }, []);
+
+
+    return (
+        <>
+            {hasPermission && <WebView
+                style={styles.container}
+                source={{ uri: 'https://malcmind.com/Web-Apps/ai-traslation' }}
+            />}
+        </>
+    );
 }
-
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
+    container: {
+        flex: 1,
+        marginTop: Constants.statusBarHeight,
+    },
 });
